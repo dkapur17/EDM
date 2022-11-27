55f9ccefd4dafdf4184645ed3dc3a79f54b2ba05
Aakash Aanegola <aakash.aanegola@gmail.com>
2022-11-27 07:36:36 +0530

    Added level select

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,24 +12,48 @@ public class MainMenu : MonoBehaviour
     private float beginning;
 
     public void PlayGame () {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Level_YouAndI");
     }
     
+    public void PlayYouAndI(){
+        SceneManager.LoadScene("Level_YouAndI");
+    }
+
+    public void PlaySamurai(){
+        SceneManager.LoadScene("Level_Samurai");    
+    }
+
+    public void PlayDioma(){
+        SceneManager.LoadScene("Level_Dioma");        
+    }
+
+    public void PlayWarMachine(){
+        SceneManager.LoadScene("Level_WarMachine");
+    }
+
+    public void PlayRedemption(){
+        SceneManager.LoadScene("Level_Redemption");
+    }
+
+    public void PlayEgo(){
+        SceneManager.LoadScene("Level_Ego");
+    }
+
     public void PlayVideo () {
         beginning = Time.time;
         videoPlayer.Play();
-        StartCoroutine(waitmethod());
+        // StartCoroutine(waitmethod());
+        Invoke("PlayGame", (float)videoPlayer.length);
     }
 
-    IEnumerator waitmethod()
-    {
-        while (Time.time - beginning < videoPlayer.length)
-        {
-            yield return null;
-        }
-        PlayGame();
-    }
+    // IEnumerator waitmethod()
+    // {
+    //     while (Time.time - beginning < videoPlayer.length)
+    //     {
+    //         yield return null;
+    //     }
+    //     PlayGame();
+    // }
 
     public void QuitGame () {
         Application.Quit();
